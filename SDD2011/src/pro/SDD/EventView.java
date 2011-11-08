79cc855bed388cd3ec2f2709f7ed0d97f00872c8
Paul F <paul66766@gmail.com>
2011-11-07 18:15:15 -0600

    Committing old changes
    Signed-off-by: Paul F <paul66766@gmail.com>

--- SDD2011/src/pro/SDD/EventView.java
+++ SDD2011/src/pro/SDD/EventView.java
@@ -1,6 +1,6 @@
 /*
  * EventView
- * 
+ *
  * Version 0.8
  *
  */
@@ -12,10 +12,15 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
 
+import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.AdapterView.OnItemClickListener;
 
 /**
  * 
@@ -32,7 +37,7 @@ public class EventView extends ListActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		setDebugTv((TextView) this.findViewById(R.id.events));
-		
+
 		try {
 
 			// Create a URL for the desired page
@@ -44,15 +49,41 @@ public class EventView extends ListActivity {
 			while ((string = bufferedreader.readLine()) != null) {
 				// append to arraylist
 				events.add(string);
-				//eventinfo.add((string = bufferedreader.readLine()));
+				eventinfo.add((string = bufferedreader.readLine()));
 			}
 			bufferedreader.close();
-			
-			//EventViewParser eventviewparser = new EventViewParser();
-			//eventviewparser.readFromWebsite();
+
+			// EventViewParser eventviewparser = new EventViewParser();
+			// eventviewparser.readFromWebsite();
 			this.setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, events));
-			//this.setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, eventviewparser.getevent()));
-			//this.setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, eventviewparser.geteventinfo()));
+			// this.setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item,
+			// eventviewparser.getevent()));
+			// this.setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item,
+			// eventviewparser.geteventinfo()));
+			this.setSelection(0);
+
+			final ListView listview = getListView();
+			listview.setTextFilterEnabled(true);
+
+			listview.setOnItemClickListener(new OnItemClickListener() {
+				// listen for click, pair event and event info (via parser, not present
+				// at the moment
+				public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+					try {
+						for (int k = 0; k < events.size(); k++) {
+							if (((TextView) view).getText() == events.get(k)) {
+								AlertDialog.Builder adb = new AlertDialog.Builder(EventView.this);
+								adb.setTitle("Event Information");
+								adb.setMessage(eventinfo.get(k));
+								adb.setPositiveButton("Ok", null);
+								adb.show();
+							}
+						}
+					} catch (Exception exception) {
+						exception.printStackTrace();
+					}
+				}
+			});
 		} catch (Exception e) {
 		}
 	}
