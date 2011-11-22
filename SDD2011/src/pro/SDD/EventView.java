9d3908eff86d6897083d4c77007e4172410ec7fc
George Woodyard <wellsley@gmail.com>
2011-11-22 02:25:45 -0600

    Updated EventView. Parser now works for multiple lines and refresh works.

--- SDD2011/src/pro/SDD/EventView.java
+++ SDD2011/src/pro/SDD/EventView.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 
 import android.app.AlertDialog;
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -48,20 +49,50 @@ public class EventView extends ListActivity {
 
 			BufferedReader bufferedreader = new BufferedReader(new InputStreamReader(url.openStream()));
 			String string;
+			String compound = "";
 
 			while ((string = bufferedreader.readLine()) != null) {
 				// append to arraylist
-				events.add(string);
-				eventinfo.add((string = bufferedreader.readLine()));
+				
+				if(string.indexOf("<eventtitle>")>-1)
+				{
+					string = string.replace("<eventtitle>","");
+					string = string.replace("</eventtitle>", "");
+					string = string.trim();
+					events.add(string);
+				}
+				if(string.indexOf("<eventinfo>")>-1)
+				{
+					string = string.replace("<eventinfo>","");
+					
+					//if </eventinfo> tag is missing, this indicates a new line
+					//is present. Split into either compound or keep string.
+					if(!(string.indexOf("</eventinfo>")>-1))
+					{
+					compound= string; //copy string into copound string
+					while(!(compound.indexOf("</eventinfo>")>-1)) //check string
+						{
+						compound= compound+bufferedreader.readLine();
+						}
+					compound = compound.replace("</eventinfo>","");
+					compound = compound.trim();
+					eventinfo.add(compound);
+					compound=""; //reset compound
+					}
+					else
+					{
+					string = string.replace("</eventinfo>","");
+					string = string.trim();
+					eventinfo.add(string);
+					}
+				}
+				//throw away <event> and </event> tags
+				string = string.replace("<event>","");
+				string = string.replace("</event>","");
+				string = string.trim();
 			}
 			bufferedreader.close();
-
-			// EventViewParser eventviewparser = new EventViewParser();
-			// eventviewparser.readFromWebsite();
 			this.setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, events));
-			//this.setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, eventviewparser.getevent()));
-			//this.setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, eventviewparser.geteventinfo()));
-
 			this.setSelection(0);
 			final ListView listview = getListView();
 			listview.setTextFilterEnabled(true);
@@ -90,8 +121,16 @@ public class EventView extends ListActivity {
 	}
 
 	private void refreshEventView() {
-		// TODO Auto-generated method stub
+		Intent intent = getIntent();
+		finish();
+		startActivity(intent);
 		
+		AlertDialog.Builder adb = new AlertDialog.Builder(EventView.this);
+		adb.setTitle("Refresh Information");
+		adb.setMessage("This page is being refreshed");
+		adb.setPositiveButton("Alright!", null);
+		adb.show();
+
 	}
 
 	public boolean onCreateOptionsMenu(Menu menu) {
